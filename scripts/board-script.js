84a4960d2db7e641d463cb0b1868da0a2acda762
Bryan Villafuerte <bryan@ordr.no>
2019-12-09 22:57:26 +0100

    added board tiles on Board page

---
+++ scripts/board-script.js
@@ -0,0 +1,137 @@
+/* The following functions are used to create board tiles and number them from 1-30 */
+function createBoard() {
+    function createBoardTiles1() {
+        var numbers = [1,2,3,4,5,6]
+        var numberInsert = "";
+        var i;
+    
+        for (i = 0; i < numbers.length; i++) {
+            var row = document.getElementById("boardContainer1");
+    
+            var boardTile = document.createElement("div");
+            boardTile.setAttribute("class", "board-tile");
+    
+            if (i % 2 === 0) {
+                boardTile.classList.add("board-tile-Odd");
+            }
+    
+            numberInsert = numbers[i];
+            var boardTileNumber = document.createElement("h2");
+            boardTileNumber.textContent = numberInsert;
+    
+            boardTile.appendChild(boardTileNumber);
+    
+            row.appendChild(boardTile);
+        }
+    }
+
+    function createBoardTiles2() {
+        var numbers = [12,11,10,9,8,7]
+        var numberInsert = "";
+        var i;
+    
+        for (i = 0; i < numbers.length; i++) {
+            var row = document.getElementById("boardContainer2");
+    
+            var boardTile = document.createElement("div");
+            boardTile.setAttribute("class", "board-tile");
+    
+            if (i % 2 !== 0) {
+                boardTile.classList.add("board-tile-Odd");
+            }
+    
+            numberInsert = numbers[i];
+            var boardTileNumber = document.createElement("h2");
+            boardTileNumber.textContent = numberInsert;
+    
+            boardTile.appendChild(boardTileNumber);
+    
+            row.appendChild(boardTile);
+        }
+    }
+
+    function createBoardTiles3() {
+        var numbers = [13,14,15,16,17,18]
+        var numberInsert = "";
+        var i;
+    
+        for (i = 0; i < numbers.length; i++) {
+            var row = document.getElementById("boardContainer3");
+    
+            var boardTile = document.createElement("div");
+            boardTile.setAttribute("class", "board-tile");
+    
+            if (i % 2 === 0) {
+                boardTile.classList.add("board-tile-Odd");
+            }
+    
+            numberInsert = numbers[i];
+            var boardTileNumber = document.createElement("h2");
+            boardTileNumber.textContent = numberInsert;
+    
+            boardTile.appendChild(boardTileNumber);
+    
+            row.appendChild(boardTile);
+        }
+    }
+
+    function createBoardTiles4() {
+        var numbers = [24,23,22,21,20,19]
+        var numberInsert = "";
+        var i;
+    
+        for (i = 0; i < numbers.length; i++) {
+            var row = document.getElementById("boardContainer4");
+    
+            var boardTile = document.createElement("div");
+            boardTile.setAttribute("class", "board-tile");
+    
+            if (i % 2 !== 0) {
+                boardTile.classList.add("board-tile-Odd");
+            }
+    
+            numberInsert = numbers[i];
+            var boardTileNumber = document.createElement("h2");
+            boardTileNumber.textContent = numberInsert;
+    
+            boardTile.appendChild(boardTileNumber);
+    
+            row.appendChild(boardTile);
+        }
+    }
+
+    function createBoardTiles5() {
+        var numbers = [25,26,27,28,29,30]
+        var numberInsert = "";
+        var i;
+    
+        for (i = 0; i < numbers.length; i++) {
+            var row = document.getElementById("boardContainer5");
+    
+            var boardTile = document.createElement("div");
+            boardTile.setAttribute("class", "board-tile");
+    
+            if (i % 2 === 0) {
+                boardTile.classList.add("board-tile-Odd");
+            }
+    
+            numberInsert = numbers[i];
+            var boardTileNumber = document.createElement("h2");
+            boardTileNumber.textContent = numberInsert;
+    
+            boardTile.appendChild(boardTileNumber);
+    
+            row.appendChild(boardTile);
+        }
+    }
+
+
+    createBoardTiles1();
+    createBoardTiles2();
+    createBoardTiles3();
+    createBoardTiles4();
+    createBoardTiles5();
+}
+
+createBoard();
+
