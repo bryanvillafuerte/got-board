c929cc0dd692b062fc55b3c419c37f461a262143
Bryan Villafuerte <bryan@ordr.no>
2019-12-14 02:08:55 +0100

    Fixed character cards
    minor tweaks to make equal card height

--- scripts/utils.js
+++ scripts/utils.js
@@ -23,14 +23,10 @@ const renderCharacters = function (characters) {
     characters.forEach(character => {
         characterRow.innerHTML += `
     <div class="col-md-3">
-        <div class="card">
-            <img 
-                style="cursor: pointer;"
-                src="${character.image}" 
-                class="card-img-top" alt="character-image" 
-                data-char-name="${character.name}">
+        <div class="card h-100">
+            <img src="${character.image}" class="card-img-top" alt="character-image" data-char-name="${character.name}">
             <div class="card-body">
-                <p class="card-text">${character.name}</p>
+                <h5 class="card-title">${character.name}</h5>
                 <p class="card-text">${character.titles}</p>
             </div>
         </div>
